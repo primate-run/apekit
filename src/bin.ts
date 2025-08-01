f712e12bc7b55f14722eafadd35bacb1bb61fa46
terrablue <102580937+terrablue@users.noreply.github.com>
2025-08-01 21:44:30 +0200

    fix issues

--- src/bin.ts
+++ src/bin.ts
@@ -3,11 +3,13 @@ import args from "@rcompat/args";
 import assert from "@rcompat/assert";
 import json from "@rcompat/package/json";
 
+const pkg_json = await json();
+
 if (args[0] === "bump") {
   const type = args[1];
   assert(["major", "minor", "patch"].includes(type),
     "can only bump major, minor or patch")
-  const package_json = await json.json() as { version: string };
+  const package_json = await pkg_json.json<{ version: string }>();
   const { version } = package_json;
 
   const match = version.match(/^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)$/);
@@ -29,7 +31,7 @@ if (args[0] === "bump") {
       patch++;
     }
     package_json.version = `${major}.${minor}.${patch}`;
-    await json.write(JSON.stringify(package_json, undefined, 2));
+    await pkg_json.writeJSON(package_json);
     console.log(`bumped ${version} -> ${package_json.version}`);
   } else {
     console.error(`couldn't match package.json's version`);
